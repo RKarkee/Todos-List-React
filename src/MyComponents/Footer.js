26b7b0f3327bb3fa305d72c233081cb6fdb019f4
RKarkee <rajankarkee01@gmail.com>
2022-03-07 17:25:38 +0545

    footer updated

--- src/MyComponents/Footer.js
+++ src/MyComponents/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
   //   width: "100%"
   // }
   return (
-    <footer className='bg-dark text-light py-3' style={footerStyle}>
+    <footer className='bg-dark text-light py-3' >
       {/* <footer className='bg-dark text-light py-3' style={footerStyle}> */}
       <p className='text-center'>
         Copyright &copy; MyTodoList.com
